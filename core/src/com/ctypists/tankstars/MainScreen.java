894135f07cba7a182ae10c62cdf2ff301a7eeb2e
Anirudh S. Kumar <61222180+Anirudh-S-Kumar@users.noreply.github.com>
2022-11-19 23:57:50 +0530

    Project Initialization

---
+++ core/src/com/ctypists/tankstars/MainScreen.java
@@ -0,0 +1,89 @@
+package com.ctypists.tankstars;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class MainScreen implements Screen {
+
+    Game game;
+    OrthographicCamera camera;
+    ExtendViewport viewport;
+    SpriteBatch batch;
+
+    public MainScreen(Game game) {
+        this.game = game;
+    }
+
+    /**
+     * Called when this screen becomes the current screen for a {@link Game}.
+     */
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        viewport = new ExtendViewport(TankStars.WIDTH, TankStars.HEIGHT, camera);
+        camera.setToOrtho(false, TankStars.WIDTH, TankStars.HEIGHT);
+    }
+
+    /**
+     * Called when the screen should render itself.
+     *
+     * @param delta The time in seconds since the last render.
+     */
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0, 1);
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+
+    }
+
+    /**
+     * @param width
+     * @param height
+     * @see ApplicationListener#resize(int, int)
+     */
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    /**
+     * @see ApplicationListener#pause()
+     */
+    @Override
+    public void pause() {
+
+    }
+
+    /**
+     * @see ApplicationListener#resume()
+     */
+    @Override
+    public void resume() {
+
+    }
+
+    /**
+     * Called when this screen is no longer the current screen for a {@link Game}.
+     */
+    @Override
+    public void hide() {
+
+    }
+
+    /**
+     * Called when this screen should release all resources.
+     */
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+}
---
+++ core/src/com/ctypists/tankstars/TankStars.java
@@ -0,0 +1,22 @@
+package com.ctypists.tankstars;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class TankStars extends Game {
+	/**
+	 * Called when the {@link Application} is first created.
+	 */
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 600;
+
+
+	@Override
+	public void create() {
+		setScreen(new MainScreen(this));
+	}
+}
